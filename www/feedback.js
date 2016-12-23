8e67c5ea388377b69c8c4536f8dc5b961601579c
joao-batista <joao.stinfo@gmail.com>
2016-12-23 14:40:00 -0300

    Create feedback.js
    Added feedback.js

---
+++ www/feedback.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var exec = require('cordova/exec');
+
+var feedback = {};
+
+feedback.send = function(options, success, failure) {
+    exec(success, failure, 'Feedback', 'send', [options]);
+};
+
+feedback.hasPermission = function(success, failure) {
+    exec(success, failure, 'Feedback', 'has_permission', []);
+};
+
+module.exports = feedback;
