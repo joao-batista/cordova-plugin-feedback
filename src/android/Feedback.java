c32741d3741405fb00da7726188291e827bfa52f
joao-batista <joao.stinfo@gmail.com>
2016-12-24 16:18:19 -0300

    commit

--- src/android/Feedback.java
+++ src/android/Feedback.java
@@ -29,12 +29,7 @@ import javax.mail.internet.MimeMessage;
 
 public class Feedback extends CordovaPlugin {
 
-    public final String ACTION_SEND = "send";
-    public final String ACTION_HAS_PERMISSION = "has_permission";
-    private static final String INTENT_FILTER = "SMS_SENT";
-    private static final int SEND_CODE = 0;
     private CallbackContext callbackContext;
-    private JSONArray args;
 
     final static int PORTA = 587;
     final static String AUTH = "true";
@@ -42,50 +37,36 @@ public class Feedback extends CordovaPlugin {
     final static String HOST = "smtp.gmail.com";
 
     @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+      super.initialize(cordova, webView);
+    }
+
+    @Override
     public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
       this.callbackContext = callbackContext;
-      this.args = args;
-      if (action.equals(ACTION_SEND)) {
-        send();
-        if (hasPermission()) {
-          send();
-        } else {
-          requestPermission();
-        }
-        return true;
-      }
-      else if (action.equals(ACTION_HAS_PERMISSION)) {
-        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, hasPermission()));
+      if ("send".equals(action)) {
+          send(args);
         return true;
       }
       return false;
     }
 
-    private boolean hasPermission() {
-      return cordova.hasPermission(android.Manifest.permission.INTERNET);
-    }
 
-    private void requestPermission() {
-      cordova.requestPermission(this, SEND_CODE, android.Manifest.permission.INTERNET);
-    }
-
-    public void send() {
+    public void send(inal JSONArray args, final CallbackContext callbackContext) {
       
       try{
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", AUTH);
         props.put("mail.smtp.starttls.enable", START_TLS);
-        String mensage = this.args.getString(0);
-        String email = this.args.getString(1);
-        String senha = this.args.getString(2);
+        String mensage = args.getString(0);
+        String email = args.getString(1);
+        String senha = args.getString(2);
 
         Session session = Session.getInstance(props);
 
         Message message = new MimeMessage(session);
-
         message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-
         message.setSubject("Memsagem do Usuário");
         message.setContent(this.args, "text/plain");
 
@@ -93,8 +74,12 @@ public class Feedback extends CordovaPlugin {
         transport.connect(HOST, PORTA, email, senha);
         transport.sendMessage(message, message.getAllRecipients());
 
+        Context context = this.cordova.getActivity().getApplicationContext();
+        Toast toast = Toast.makeText(context, "email enviado", Toast.LENGTH_SHORT);
+        toast.show();
+
       } catch (Exception e) {
-        //LOG.e("EmailComposer", "Error handling subject param: " + e.toString());
+        
       }
 
     }
