64f687c5860497c3a147c4a38ad2937011eb903f
joao-batista <joao.stinfo@gmail.com>
2016-12-24 16:43:09 -0300

    commit

--- www/feedback.js
+++ www/feedback.js
@@ -1,26 +1,7 @@
-function Feedback() {
-	this.resultCallback = null;
-}
+var feedback = {};
 
-Feedback.prototype.send = function(options, success, failure) {
-	alert(options);
-  cordova.exec(success, failure, 'Feedback', 'send', [options]);
+feedback.send = function(options, success, failure) {
+    cordova.exec(success, failure, "Feedback", "send", [options]);
 };
 
-Feedback.prototype.hasPermission = function(success, failure) {
-  cordova.exec(success, failure, 'Feedback', 'has_permission', []);
-};
-
-cordova.addConstructor(function() {
-
-	if (!window.plugins) {
-		window.plugins = {};
-	}
-
-	if (!window.Cordova) {
-		window.Cordova = cordova;
-	}
-	;
-
-	window.plugins.feedback = new Feedback();
-});
+module.exports = feedback;
