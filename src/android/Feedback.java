bcffb999a051ec090eb4837231c87efc5d198e75
joao-batista <joao.stinfo@gmail.com>
2016-12-25 00:32:00 -0300

    commit

--- src/android/Feedback.java
+++ src/android/Feedback.java
@@ -25,107 +25,63 @@ import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import org.apache.commons.mail.SimpleEmail;
-import org.apache.commons.mail.EmailException;
 
 
 public class Feedback extends CordovaPlugin {
 
-    private CallbackContext callbackContext;
-/*    private SimpleEmail email;
+  private CallbackContext callbackContext;
 
-    final static int PORTA = 587;
-    final static String AUTH = "true";
-    final static String START_TLS = "true";
-    final static String HOST = "smtp.gmail.com";*/
+  final static int PORTA = 587;
+  final static String AUTH = "true";
+  final static String START_TLS = "true";
+  final static String HOST = "smtp.gmail.com";
 
-    @Override
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-      super.initialize(cordova, webView);
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+  }
+
+  @Override
+  public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
+    this.callbackContext = callbackContext;
+    if ("send".equals(action)) {
+      send(args, callbackContext);
+      return true;
     }
+    return false;
+  }
 
-    @Override
-    public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
-      this.callbackContext = callbackContext;
-      if ("send".equals(action)) {
-          send(args, callbackContext);
-        return true;
-      }
-      return false;
-    }
 
-    public void send(final JSONArray args, final CallbackContext callbackContext) {
+  public void send(final JSONArray args, final CallbackContext callbackContext) {
     
-        try {
-           JSONObject props = args.getJSONObject(0);
-           SimpleEmail email = setEmailProperties(props);
-           /*email.setHostName("smtp.gmail.com");
-           email.setSmtpPort(465);
-           email.addTo("xxx@xxx.com", "Jose");
-           email.setFrom("seuemail@seuprovedor.com", "Seu nome");
-           email.setSubject("Test message");
-           email.setMsg("This is a simple test of commons-email");
-           email.setSSL(true);
-           email.setAuthentication("username", "senha");*/
-           email.send();
-        } catch (Exception e) {
+    try{
 
-        }
+      Properties props = new Properties();
+      props.put("mail.smtp.auth", AUTH);
+      props.put("mail.smtp.starttls.enable", START_TLS);
+
+      String mensage = args.getString(0);
+      String email   = args.getString(1);
+      String senha   = args.getString(2);
+
+      Session session = Session.getInstance(props);
+      Message message = new MimeMessage(session);
+      message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+      message.setSubject("Memsagem do Usuário");
+      message.setContent("mensage", "text/html");
+
+      Transport transport = session.getTransport("smtp");
+      transport.connect(HOST, PORTA, email, senha);
+      transport.sendMessage(message, message.getAllRecipients());
+
+      Context context = this.cordova.getActivity().getApplicationContext();
+      Toast toast = Toast.makeText(context, "email enviado", Toast.LENGTH_SHORT);
+      toast.show();
+
+    } catch (Exception e) {
+      callbackContext.error(e.getMessage());
     }
 
-    public SimpleEmail setEmailProperties (JSONObject params) throws JSONException, EmailException {
-
-        SimpleEmail email = new SimpleEmail();
-
-        if (params.has("host"))
-            email.setHostName(params.getString("host"));
-        if (params.has("port"))
-            email.setSmtpPort(params.getInt("port"));
-        if (params.has("to"))
-            email.addTo(params.getString("to"), "Seu nome");
-        if (params.has("from"))
-            email.setFrom(params.getString("from"), "Seu nome");
-        if (params.has("subject"))
-            email.setSubject(params.getString("subject"));
-        if (params.has("msg"))
-            email.setMsg(params.getString("msg"));
-        if(params.has("username") && params.has("password"))
-            email.setAuthentication(params.getString("username"), params.getString("password"));
-
-        return email;
-    }
-
-
-    /*public void send(final JSONArray args, final CallbackContext callbackContext) {
-      
-      try{
-        Context context = this.cordova.getActivity().getApplicationContext();
-
-        String mensage = args.getString(0);
-        String email = args.getString(1);
-        String senha = args.getString(2);
-
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-
-        Session session = Session.getInstance(props);
-        Message message = new MimeMessage(session);
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-        message.setSubject("Memsagem do Usuário");
-        message.setContent(mensage, "text/html");
-
-        Transport transport = session.getTransport("smtp");
-        transport.connect(HOST, PORTA, email, senha);
-        transport.sendMessage(message, message.getAllRecipients());
-
-        Toast toast = Toast.makeText(context, "email enviado", Toast.LENGTH_SHORT);
-        toast.show();
-
-      } catch (Exception e) {
-        callbackContext.error(e.getMessage());
-      }
-
-    }*/
+  }
   
 }
